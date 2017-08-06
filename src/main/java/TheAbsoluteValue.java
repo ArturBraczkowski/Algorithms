cea6900a5aecabf9c0ceb043bf2618705bcba271
Artur <arturbra@gmail.com>
2017-08-06 11:17:57 +0200

    + the absolute value

---
+++ src/main/java/TheAbsoluteValue.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Created by artur on 06.08.2017.
+ */
+public class TheAbsoluteValue {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        int number;
+        int result;
+
+        System.out.println("Podaj liczbe");
+        number = myScanner.nextInt();
+
+        result = getAbsolute (number);
+
+        System.out.println("wynik to: " + result);
+    }
+    public static int getAbsolute(int number) {
+        int result;
+        if(number > 0) {
+            result = number;
+
+        }
+        else {
+            result = number * -1;
+        }
+        return result;
+    }
+
+}
