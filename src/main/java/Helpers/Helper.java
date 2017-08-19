fe6cb3b34f54aab19b917dd79c076ac68e9db3d3
Artur <arturbra@gmail.com>
2017-08-19 12:13:04 +0200

    + Edit Helper + Erastotenes

--- src/main/java/Helpers/Helper.java
+++ src/main/java/Helpers/Helper.java
@@ -6,23 +6,38 @@ import java.util.Scanner;
  * Created by artur on 19.08.2017.
  */
 public class Helper {
-    public static int [] getArrayFromUser () {
+    public static int[] getArrayFromUser() {
 
-            System.out.println("Podaj wielkość tablicy: ");
-            Scanner myScanner = new Scanner(System.in);
-            int n = myScanner.nextInt();
+        System.out.println("Podaj wielkość tablicy: ");
+        Scanner myScanner = new Scanner(System.in);
+        int n = myScanner.nextInt();
 
-            int [] arrayN = new int [n];
+        int[] arrayN = new int[n];
 
-            for (int i = 0; i < arrayN.length; i++) {
-                System.out.println("Podaj kolejną liczbę");
-                arrayN[i] = myScanner.nextInt();
+        for (int i = 0; i < arrayN.length; i++) {
+            System.out.println("Podaj kolejną liczbę");
+            arrayN[i] = myScanner.nextInt();
 
-            }return arrayN;
+        }
+        return arrayN;
     }
 
-    public static void printArray (int[] arrayN) {
-        for (int i = 0; i <arrayN.length ; i++) {
+    public static int[] getArrayFromUserBySize() {
+        System.out.println("Podaj wielkosc tablicy");
+        Scanner myScanner = new Scanner(System.in);
+        int n = myScanner.nextInt();
+
+        int[] arrayN = new int[n - 1];
+
+        for (int i = 0; i < arrayN.length; i++) {
+            arrayN[i] = i + 2;
+
+        }
+        return arrayN;
+    }
+
+    public static void printArray(int[] arrayN) {
+        for (int i = 0; i < arrayN.length; i++) {
             System.out.println(arrayN[i]);
 
         }
