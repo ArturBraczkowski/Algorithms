5d4f16f2ca5ea61115271e8df648ae88eefd2b62
Artur <arturbra@gmail.com>
2017-08-20 11:23:13 +0200

    + Queue + InvokeQueue + QueueElement + QueueOnArray

---
+++ src/main/java/Queue/Queue.java
@@ -0,0 +1,76 @@
+package Queue;
+
+import Helpers.Helper;
+
+/**
+ * Created by artur on 20.08.2017.
+ */
+
+public class Queue {
+
+    private QueueElement first = null;
+    private QueueElement last = null;
+
+    public void addToQueue(int value) {
+
+        QueueElement item = new QueueElement(value);
+
+        if (this.first == null) {
+            this.first = item;
+        }
+        if (last != null) {
+            this.last.setNext(item);
+        }
+        this.last = item;
+    }
+
+    public int removeFromQueue()
+    {
+        if (this.first != null) {
+            int temp = this.first.getValue();
+            this.first = first.getNext();
+            return temp;
+        }
+        return -1;
+    }
+
+    public boolean isEmpty()
+    {
+        if (this.last == null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int getPickOfQueue()
+    {
+        if (this.first != null) {
+            return this.first.getValue();
+        }
+
+        return -1;
+    }
+
+    public int getLastAdded()
+    {
+        if (this.last != null) {
+            return this.last.getValue();
+        }
+
+        return -1;
+    }
+
+    public void printQueue()
+    {
+        if (this.first != null) {
+            QueueElement temp = this.first;
+            while(temp.getNext() != null) {
+                Helper.printInt(temp.getValue());
+                temp = temp.getNext();
+            }
+            Helper.printInt(temp.getValue());
+        }
+
+    }
+}
